0d240818d337ae7d428dbac5a62fea19c5d58fd6
Davvos11 <vosdavid2@gmail.com>
2020-11-24 14:15:35 +0100

    added database connection

---
+++ create-tables.sql
@@ -0,0 +1,11 @@
+create table if not exists users
+(
+	username text not null
+		constraint users_pk
+			primary key,
+	password text,
+	hash_iterations int default 5000 not null,
+	server_salt text,
+	client_salt text
+);
+
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log("test")
+import app from "./express.js"
+import {login} from "./authentication.js";
+import {DbConnection} from "./database.js";
+
+const PORT = 8000
+
+// Login page
+app.post('/login', login)
+
+// Connect to db
+const db = new DbConnection()
+db.getDb().then(() => {
+    console.log("Connected to db")})
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`)
+})
